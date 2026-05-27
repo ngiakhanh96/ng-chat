fadcccf63f0073281cac90667013632bb992dc99
Khanh <khanh@example.com>
2026-05-28 00:37:12 +0800

    chore: cleanup

--- libs/shared/data-access/src/lib/models/app-settings.ts
+++ libs/shared/data-access/src/lib/models/app-settings.ts
@@ -1,8 +1,5 @@
 export interface IAppSettings {
   googleAuthApiBaseUrl: string;
-  invidiousApiBaseUrl: string;
-  youtubeApiBaseUrl: string;
-  ytDlpApiBaseUrl: string;
   regionCode: string;
   languageCode: string;
   supportedSocialMedias: Record<string, string>;
