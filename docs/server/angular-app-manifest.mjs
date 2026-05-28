4f92df900ac64c01a200fe243ccb64db9d5815a0
Khanh <khanh@example.com>
2026-05-29 00:05:55 +0800

    refactor: update stylesheets and remove deprecated files
    - Updated references in HTML files to use new stylesheet names.
    - Removed old stylesheet `styles-DFOEYH5U.css` and added new stylesheet `styles-P2JMIFK5.css`.
    - Adjusted SCSS to reference Tailwind CSS instead of the previous app stylesheet.

--- docs/server/angular-app-manifest.mjs
+++ docs/server/angular-app-manifest.mjs
@@ -20,9 +20,9 @@ export default {
 ],
   entryPointToBrowserMapping: undefined,
   assets: {
-    'index.csr.html': {size: 5288, hash: 'e67a8ca83dfd51c0d9f61417b6b185bf3f0a70b7c2c81e09ef9489678862b3ad', text: () => import('./assets-chunks/index_csr_html.mjs').then(m => m.default)},
-    'index.server.html': {size: 1073, hash: 'c367e78f4f5b2ccb95e8384de63552bab60a0c82ab675ca93542a4f5da98a719', text: () => import('./assets-chunks/index_server_html.mjs').then(m => m.default)},
-    'index.html': {size: 13543, hash: '7b9958a70f1e6e6eace3005c68297b846d68f3a27f7484b18f7768f388b513e6', text: () => import('./assets-chunks/index_html.mjs').then(m => m.default)},
-    'styles-DFOEYH5U.css': {size: 8764, hash: 'a3vj6YOgZz0', text: () => import('./assets-chunks/styles-DFOEYH5U_css.mjs').then(m => m.default)}
+    'index.csr.html': {size: 5395, hash: '6e087c4e3ff5529d854ff8e793c225a9eb5e8589d7227d52dbeb073e7e961916', text: () => import('./assets-chunks/index_csr_html.mjs').then(m => m.default)},
+    'index.server.html': {size: 1073, hash: '54435c1df6334b3e7eef6777a4f0e727dd2b76806849715dfba2b95b059c7f89', text: () => import('./assets-chunks/index_server_html.mjs').then(m => m.default)},
+    'index.html': {size: 13650, hash: '83324f28a24d3abbf4744bb7ef5ad9bc2cbf40539ce45da10393e31762e6bf5a', text: () => import('./assets-chunks/index_html.mjs').then(m => m.default)},
+    'styles-P2JMIFK5.css': {size: 9252, hash: '6QbEMA1d0yo', text: () => import('./assets-chunks/styles-P2JMIFK5_css.mjs').then(m => m.default)}
   },
 };
